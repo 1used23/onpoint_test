b67a90affb0c9577886d746588f9bde1a1339142
myzner <39660168+myzner@users.noreply.github.com>
2019-07-11 01:21:19 +0300

    Update nav.js

--- nav.js
+++ nav.js
@@ -3,19 +3,19 @@ let touchStartY;
 let touchEndY;
 let currentSlide = 0;
 
-const update = function(state){
-// 	document.getElementById('scroll-down').style.display = "table";
-	 document.getElementById('scroll-down').style.animationName = "none";
+const update = function(state) {
 	document.querySelector('.nav__0').style.backgroundColor = 'white';
 	document.querySelector('.nav__1').style.backgroundColor = 'white';
 	document.querySelector('.nav__2').style.backgroundColor = 'white';
   	slideWrapper.style.transform = `translateY(-${state*768}px)`;
-  	console.log('.nav__' + state);
 	document.querySelector('.nav__' + state).style.backgroundColor = '#f78b1f';
 
-	 if (state == 2){
-	 	document.getElementById('scroll-down').style.animationName = "pass-out";
+	 if (state == 2) {
+	 	document.getElementById('scroll-down').style.opacity = 0;
+	 } else if (state == 1) {
+	 	document.getElementById('scroll-down').style.opacity = 1;
 	 }
+
 }
 
 window.addEventListener('touchstart', e => {
@@ -24,11 +24,11 @@ window.addEventListener('touchstart', e => {
 
 window.addEventListener('touchend', e => {
 	touchEndY = e.changedTouches[0].clientY;
-	if(touchStartY - touchEndY > 50 && currentSlide <= 1) {
+	if (touchStartY - touchEndY > 50 && currentSlide <= 1) {
 	  currentSlide += 1;
 	  update(currentSlide);
 	}
-	if(touchStartY - touchEndY < -50 && currentSlide >= 1) {
+	if (touchStartY - touchEndY < -50 && currentSlide >= 1) {
 	 currentSlide -= 1;
 	 update(currentSlide);
 	}
