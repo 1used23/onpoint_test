25ac016955e079bf1f069f76d79f44e4630c746f
myzner <39660168+myzner@users.noreply.github.com>
2019-07-11 10:23:57 +0300

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,18 +1,21 @@
 const slideWrapper = document.querySelector('.slide-wrapper');
+const nav_0 = document.querySelector('.nav__0');
+const nav_1 = document.querySelector('.nav__1');
+const nav_2 = document.querySelector('.nav__2');
 let touchStartY;
 let touchEndY;
 let currentSlide = 0;
 
-const update = function(state) {
-	document.querySelector('.nav__0').style.backgroundColor = 'white';
-	document.querySelector('.nav__1').style.backgroundColor = 'white';
-	document.querySelector('.nav__2').style.backgroundColor = 'white';
-  	slideWrapper.style.transform = `translateY(-${state*768}px)`;
-	document.querySelector('.nav__' + state).style.backgroundColor = '#f78b1f';
+const update = function(currentSlide) {
+	nav_0.style.backgroundColor = 'white';
+	nav_1.style.backgroundColor = 'white';
+	nav_2.style.backgroundColor = 'white';
+  	slideWrapper.style.transform = `translateY(-${currentSlide*768}px)`;
+	document.querySelector('.nav__' + currentSlide).style.backgroundColor = '#f78b1f';
 
-	 if (state == 2) {
+	 if (currentSlide == 2) {
 	 	document.getElementById('scroll-down').style.opacity = 0;
-	 } else if (state == 1) {
+	 } else if (currentSlide == 1) {
 	 	document.getElementById('scroll-down').style.opacity = 1;
 	 }
 
